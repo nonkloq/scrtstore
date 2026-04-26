21fa84f8832c939a9236c2f7e40e908212eb951e
satz <117766410+nonkloq@users.noreply.github.com>
2026-04-26 19:57:15 +0530

    Add page tree

--- README.md
+++ README.md
@@ -3,8 +3,4 @@
 - [x] Encryption decryption utility methods
 - [x] Password 2FA, main key encryption setup
 - [x] Custom file format to store data `*.scrt` package
-- [-] TUI
-  - [ ] Login window
-  - [ ] Vault window
-- [ ] Very secrete `*.scrt`
-- [ ] Move to store the `*.scrt` file in cloud storage (google/dropbox/github/etc...)
+- [ ] TUI
--- tui/src/main.rs
+++ tui/src/main.rs
@@ -1,17 +1,49 @@
 use core::fmt;
-use gui::pages::Pages;
-use gui::pages::select_store::StoresList;
-use ratatui::style::Color;
-
-use crossterm::event::{self, Event};
-use ratatui::style::Style;
+use crossterm::event::DisableMouseCapture;
+use crossterm::event::EnableMouseCapture;
+use crossterm::event::KeyCode;
+use crossterm::event::{self, Event, KeyEvent, MouseEvent};
+use crossterm::{
+    cursor::{Hide, Show},
+    execute,
+};
+use ratatui::layout::Alignment;
+use ratatui::layout::Constraint;
+use ratatui::layout::Layout;
+use ratatui::layout::Rect;
 use ratatui::widgets::Block;
+use ratatui::widgets::Borders;
+use ratatui::widgets::Paragraph;
+use ratatui::widgets::Wrap;
 use ratatui::{DefaultTerminal, Frame};
+use std::cell::RefCell;
+use std::io::stdout;
+use std::rc::Rc;
+use std::rc::Weak;
 
-struct App {
-    is_exit: bool,
-    is_in_background: bool,
-    page: Pages,
+struct CursorGuard;
+
+impl CursorGuard {
+    pub fn new() -> Result<Self, AppError> {
+        execute!(stdout(), Hide)?;
+        Ok(Self)
+    }
+}
+
+impl Drop for CursorGuard {
+    fn drop(&mut self) {
+        let _ = execute!(stdout(), Show);
+    }
+}
+
+fn enable_mouse() -> std::result::Result<(), AppError> {
+    execute!(stdout(), EnableMouseCapture)?;
+    Ok(())
+}
+
+fn disable_mouse() -> std::result::Result<(), AppError> {
+    execute!(stdout(), DisableMouseCapture)?;
+    Ok(())
 }
 
 #[derive(Debug)]
@@ -19,6 +51,7 @@ enum AppError {
     AnyError(String),
     IOError(std::io::Error),
 }
+
 impl From<std::io::Error> for AppError {
     fn from(value: std::io::Error) -> Self {
         AppError::IOError(value)
@@ -38,90 +71,229 @@ impl fmt::Display for AppError {
         }
     }
 }
+//
+// trait TUIApp {
+//     // Event Handlers
+//     fn handle_key_event(&mut self, event: KeyEvent) -> Result<(), AppError>;
+//     fn handle_mouse_event(&mut self, event: MouseEvent) -> Result<(), AppError>;
+//     fn handle_paste_event(&mut self, content: String) -> Result<(), AppError>;
+//     fn handle_focus_lost_event(&mut self) -> Result<(), AppError>;
+//     fn handle_focus_gain_event(&mut self) -> Result<(), AppError>;
+//     fn handle_resize_event(&mut self, x: u16, y: u16) -> Result<(), AppError>;
+//
+//     fn handle_events(&mut self) -> Result<(), AppError> {
+//         let event = event::read()?;
+//
+//         match event {
+//             Event::Key(event) => self.handle_key_event(event),
+//             Event::Mouse(event) => self.handle_mouse_event(event),
+//             Event::Paste(content) => self.handle_paste_event(content),
+//             Event::Resize(x, y) => self.handle_resize_event(x, y),
+//             Event::FocusLost => self.handle_focus_lost_event(),
+//             Event::FocusGained => self.handle_focus_gain_event(),
+//         }
+//     }
+//
+//     /// If True the app will run; otherwise will close
+//     fn is_running(&self) -> bool;
+//
+//     /// Main render block
+//     fn render(&self, frame: &mut Frame);
+//
+//     fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<(), AppError> {
+//         while self.is_running() {
+//             terminal.draw(|frame| self.render(frame))?;
+//             // It blocks the loop to listen for events
+//             self.handle_events()? // blocks (waits)
+//         }
+//         Ok(())
+//     }
+// }
 
-impl App {
-    fn new() -> Self {
-        App {
-            is_exit: false,
-            is_in_background: false,
-            page: Pages::ListStores(StoresList::default()),
+enum ChildNode<'a> {
+    Section(Rc<RefCell<SectionNode<'a>>>),
+    Widget(Rc<RefCell<Box<dyn WidgetNode + 'a>>>),
+}
+
+impl ChildNode<'_> {
+    fn get_section_length(&self) -> u16 {
+        match self {
+            ChildNode::Section(sec) => sec.borrow().get_total_length(),
+            ChildNode::Widget(wid) => wid.borrow().get_length(),
         }
     }
 
-    fn render(&mut self, frame: &mut Frame) {
-        if self.is_in_background {
-            frame.render_widget(
-                Block::default().style(Style::default().bg(Color::DarkGray)),
-                frame.area(),
-            );
-        } else {
-            self.page.render(frame);
+    fn render(&self, frame: &mut Frame, area: Rect) {
+        match self {
+            ChildNode::Section(sec) => sec.borrow().render_section(area, frame),
+            ChildNode::Widget(wid) => wid.borrow().render_widget(area, frame),
         }
-    }
-
-    fn handle_events(&mut self) -> Result<(), AppError> {
-        let event = event::read()?;
-
-        if self.is_in_background {
-            // Key/Mouse event triggered it to foreground
-            if matches!(event, Event::Key(_) | Event::Mouse(_)) {
-                self.is_in_background = false;
-            }
-            return Ok(());
-        }
-
-        let state = match event {
-            Event::Key(_) | Event::Mouse(_) | Event::Paste(_) => self.page.handle_event(event),
-            Event::Resize(_x, _y) => Ok(None),
-            Event::FocusLost => {
-                self.is_in_background = true;
-                Ok(None)
-            }
-            Event::FocusGained => {
-                self.is_in_background = false;
-                Ok(None)
-            }
-        }?;
-
-        if let Some(page) = state {
-            match page {
-                Pages::HideScreen => self.is_in_background = true,
-                // TEMP: quit instead of a dialog
-                Pages::QuitScreen => self.is_exit = true,
-                _ => self.page = page,
-            }
-        }
-
-        Ok(())
-    }
-
-    fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<(), AppError> {
-        while !self.is_exit {
-            terminal.draw(|frame| self.render(frame))?;
-            // It blocks the loop to listen for events
-            self.handle_events()? // blocks (waits)
-        }
-        Ok(())
     }
 }
 
-// HACK: Temporary Cursor gaurd, need to modify this
-struct CursorGuard;
-
-impl CursorGuard {
-    fn new() -> Self {
-        print!("\x1b[5 q");
-        CursorGuard
+impl<'a> ChildNode<'a> {
+    fn new_widget<T: WidgetNode + 'a>(widget: T) -> Self {
+        ChildNode::Widget(Rc::new(RefCell::new(Box::new(widget))))
     }
 }
 
-impl Drop for CursorGuard {
-    fn drop(&mut self) {
-        print!("\x1b[1 q");
+trait WidgetNode {
+    fn render_widget(&self, area: Rect, frame: &mut Frame);
+    fn get_length(&self) -> u16;
+}
+
+struct SectionNode<'a> {
+    children: Vec<ChildNode<'a>>,
+    parent: Option<Weak<RefCell<SectionNode<'a>>>>,
+}
+
+impl<'a> SectionNode<'a> {
+    fn new(children: Vec<ChildNode<'a>>) -> Rc<RefCell<Self>> {
+        let node = Rc::new(RefCell::new(Self {
+            children: vec![],
+            parent: None,
+        }));
+        // Add current node as parent for each children sections
+        children.iter().for_each(|child| {
+            if let ChildNode::Section(sec) = &child {
+                sec.borrow_mut().parent = Some(Rc::downgrade(&node));
+            }
+        });
+        node.borrow_mut().children.extend(children);
+        node
     }
+}
+
+impl SectionNode<'_> {
+    fn get_total_length(&self) -> u16 {
+        self.children.iter().map(|c| c.get_section_length()).sum()
+    }
+
+    fn render_section(&self, area: Rect, frame: &mut Frame) {
+        // Draw section outline
+        let block = Block::default().borders(Borders::ALL);
+        let inner = block.inner(area);
+        frame.render_widget(block, area);
+
+        // Draw sections
+        let constraints = self
+            .children
+            .iter()
+            .map(|c| Constraint::Length(c.get_section_length()));
+
+        Layout::default()
+            .direction(ratatui::layout::Direction::Vertical)
+            .constraints(constraints)
+            .split(inner)
+            .iter()
+            .enumerate()
+            .for_each(|(idx, sub_area)| self.children[idx].render(frame, *sub_area));
+    }
+}
+
+struct PageTree<'a> {
+    root: Rc<RefCell<SectionNode<'a>>>,
+    // current_selection: Option<Weak<RefCell<SectionNode<'a>>>>,
+}
+
+impl<'a> PageTree<'a> {
+    fn new(root: Rc<RefCell<SectionNode<'a>>>) -> Self {
+        Self {
+            root,
+            // current_selection: None,
+        }
+    }
+}
+
+impl PageTree<'_> {
+    fn render(&self, frame: &mut Frame) {
+        let area = frame.area();
+        self.root.borrow().render_section(area, frame);
+    }
+}
+
+struct TextBlock {
+    name: String,
+    len: u16,
+}
+impl TextBlock {
+    fn new(string: &str, len: u16) -> Self {
+        TextBlock {
+            name: string.to_string(),
+            len,
+        }
+    }
+}
+impl WidgetNode for TextBlock {
+    fn get_length(&self) -> u16 {
+        self.len
+    }
+    fn render_widget(&self, area: Rect, frame: &mut Frame) {
+        let p = Paragraph::new(self.name.clone())
+            .block(Block::default().title("Text Block").borders(Borders::ALL))
+            .alignment(Alignment::Center)
+            .wrap(Wrap { trim: true });
+
+        frame.render_widget(p, area);
+    }
+}
+//
+// struct MyApp {
+//     is_alive: bool,
+// }
+//
+// impl MyApp {
+//     fn new() -> Self {
+//         MyApp { is_alive: true }
+//     }
+// }
+
+fn run(term: &mut DefaultTerminal, tree: PageTree) -> Result<(), AppError> {
+    loop {
+        term.draw(|frame| {
+            tree.render(frame);
+        })?;
+
+        if let Event::Key(e) = event::read()? {
+            match e.code {
+                KeyCode::Char('q') => break, // Exit the loop
+                _ => {}
+            }
+        }
+    }
+    Ok(())
 }
 
 fn main() {
-    let _cur = CursorGuard::new();
-    let _app_result = ratatui::run(|terminal| App::new().run(terminal));
+    // let _ = disable_mouse();
+    // let _ = enable_mouse();
+    let l11 = ChildNode::new_widget(TextBlock::new("At First 1", 3));
+    let l12 = ChildNode::new_widget(TextBlock::new("At First 2", 3));
+
+    let l2s = SectionNode::new(Vec::from([
+        ChildNode::new_widget(TextBlock::new("At second 1", 5)),
+        ChildNode::new_widget(TextBlock::new("At second 2", 5)),
+    ]));
+
+    let l3s = SectionNode::new(Vec::from([
+        ChildNode::new_widget(TextBlock::new("At Third 1", 4)),
+        ChildNode::new_widget(TextBlock::new("At Third 2", 5)),
+        ChildNode::new_widget(TextBlock::new("At Third 3", 4)),
+    ]));
+
+    let major1 = SectionNode::new(Vec::from([l11, l12, ChildNode::Section(l2s)]));
+    let minor1 = SectionNode::new(Vec::from([
+        ChildNode::Section(l3s),
+        ChildNode::new_widget(TextBlock::new("Ligma", 3)),
+    ]));
+
+    let page_root = SectionNode::new(Vec::from([
+        ChildNode::new_widget(TextBlock::new("It Supposed to be the heading...", 3)),
+        ChildNode::Section(major1),
+        ChildNode::Section(minor1),
+    ]));
+
+    let tree = PageTree::new(Rc::clone(&page_root));
+    // let _cur = CursorGuard::new();
+    let _app_result = ratatui::run(|terminal| run(terminal, tree));
 }
